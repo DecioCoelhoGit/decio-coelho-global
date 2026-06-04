1aec4748ce9d862c687e87608c3111336cba1bd4
Antonio Décio Ferreira Coelho <complexoturisticoviracopos@gmail.com>
2026-06-04 17:08:33 -0400

    Add async function to load dashboard data

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,28 @@
+async function carregarDashboard() {
 
+ const resposta =
+ await fetch('assets/data/dashboard.json');
+
+ const dados =
+ await resposta.json();
+
+ document.getElementById("territorios")
+ .innerText = dados.territorios;
+
+ document.getElementById("projetos")
+ .innerText = dados.projetos;
+
+ document.getElementById("hologramas")
+ .innerText = dados.hologramas;
+
+ document.getElementById("ecossistemas")
+ .innerText = dados.ecossistemas;
+
+ document.getElementById("agentes")
+ .innerText = dados.agentes;
+
+ document.getElementById("status")
+ .innerText = dados.status;
+}
+
+carregarDashboard();
