fd9da0d10d378f9edb57e046d2523a0a9ac9a36c
Antonio Décio Ferreira Coelho <complexoturisticoviracopos@gmail.com>
2026-07-04 04:10:11 -0400

    Add barrel export for Cognitive Object Model

---
+++ dcglobal-ai/shared/models/cognitive-object/index.ts
@@ -0,0 +1,14 @@
+/**
+ * DCGLOBAL.AI™
+ * Cognitive Object Model™ — COM
+ *
+ * Barrel export oficial do DNA Semântico
+ * da Engenharia do Conhecimento Vivo™.
+ */
+
+export * from "./cognitive-object";
+export * from "./cognitive-object.types";
+export * from "./cognitive-object.schema";
+export * from "./cognitive-object.factory";
+export * from "./cognitive-object.validator";
+export * from "./cognitive-object.lifecycle";
