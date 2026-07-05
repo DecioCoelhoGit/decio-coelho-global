d48873309e1af8c29ad0a45d82a949de41920514
Antonio Décio Ferreira Coelho <complexoturisticoviracopos@gmail.com>
2026-07-05 15:52:00 -0400

    Create governance.index.ts

---
+++ dcglobal-ai/core/governance/governance.index.ts
@@ -0,0 +1,21 @@
+/**
+ * DCGLOBAL.AI™
+ * Cognitive Governance™
+ *
+ * Barrel Export oficial da
+ * Camada de Governança Cognitiva.
+ */
+
+export * from "./governance.types";
+export * from "./governance.config";
+export * from "./governance.registry";
+export * from "./governance.lifecycle";
+export * from "./governance.policies";
+export * from "./governance.rules";
+export * from "./governance.audit";
+export * from "./governance.risk";
+export * from "./governance.approval";
+export * from "./governance.compliance";
+export * from "./governance.monitor";
+export * from "./governance.health";
+export * from "./governance.bootstrap";
