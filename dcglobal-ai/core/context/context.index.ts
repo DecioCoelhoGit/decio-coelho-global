c7cab9a057c9a6281b33c7b3eae55be843a26488
Antonio Décio Ferreira Coelho <complexoturisticoviracopos@gmail.com>
2026-07-05 11:38:26 -0400

    Create context.index.ts

---
+++ dcglobal-ai/core/context/context.index.ts
@@ -0,0 +1,15 @@
+/**
+ * DCGLOBAL.AI™
+ * Cognitive Context™
+ *
+ * Barrel Export oficial da
+ * Camada de Consciência Contextual.
+ */
+
+export * from "./context.types";
+export * from "./context.config";
+export * from "./context.registry";
+export * from "./context.lifecycle";
+export * from "./context.store";
+export * from "./context.cache";
+export * from "./context.search";
