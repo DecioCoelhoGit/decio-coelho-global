aebbfd1e647b8360f03481d188ce93f023f90bf2
Antonio Décio Ferreira Coelho <complexoturisticoviracopos@gmail.com>
2026-07-06 23:12:50 -0400

    Create index.ts for Knowledge module exports
    Add index file to export knowledge module components.

---
+++ dcglobal-ai/core/knowledge/index.ts
@@ -0,0 +1,23 @@
+/**
+ * DCGLOBAL.AI™
+ * Cognitive Knowledge™
+ *
+ * Exportações oficiais
+ * da Camada Knowledge.
+ */
+
+export * from "./knowledge.types";
+export * from "./knowledge.config";
+export * from "./knowledge.registry";
+export * from "./knowledge.lifecycle";
+export * from "./knowledge.store";
+export * from "./knowledge.cache";
+export * from "./knowledge.search";
+export * from "./knowledge.index";
+export * from "./knowledge.audit";
+export * from "./knowledge.monitor";
+export * from "./knowledge.health";
+export * from "./knowledge.persistence";
+export * from "./knowledge.snapshot";
+export * from "./knowledge.graph";
+export * from "./knowledge.bootstrap";
